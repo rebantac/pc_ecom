bba118edb82938448218e6669d7ad5f95247efa7
Mritunjay Kumar <83032964+Mritunjay1711@users.noreply.github.com>
2023-01-10 20:16:56 +0530

    ProductCard

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+  let { name, description, imageUrl, price } = props;
   return (
-    <div>
-      
+    <div className='mx-3 my-3'>
+      <div className="card" style={{ width: '18rem' }}>
+        <img className="card-img-top" src={!imageUrl?"Image not found": imageUrl} />
+        <div className="card-body">
+          <p className="card-text">${name}</p>
+        </div>
+      </div>
     </div>
   )
 }
