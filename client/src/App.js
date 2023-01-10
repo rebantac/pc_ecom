3a28a625a8cab22c8ce6bd642f4a0cfff5cbae9f
Rebanta Chakraborty <rebantachakraborty8@gmail.com>
2023-01-11 00:02:41 +0530

    bug fix 1

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/productCard' element={<ProductCard/>}/>
-        <Route path='/product/:id' element={<PdtDetail />} /> 
+        <Route path='/product' element={<PdtDetail />} /> 
       </Routes>
 
       <Footer />
