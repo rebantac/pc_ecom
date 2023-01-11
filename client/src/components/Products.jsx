2a07cccbaaa1ab0e5c6cf8148726746e0f68fa28
Mritunjay Kumar <83032964+Mritunjay1711@users.noreply.github.com>
2023-01-11 20:26:49 +0530

    updated navbar

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import data from '../assets/data.json'
 import ProductCard from './ProductCard'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 
 function Products() {
-    console.log(data);
     return (
         <div className="container my-3">
             <div className="row">
-                <h1>Fetched data</h1>
+                <h1>Our Products</h1>
                 {data && data.map((element) => {
                     // return <Link to={<ProductDetails key={element.Id}/>}><div className="col-md-3 my-3 mx-3">
                     //     <ProductCard key={element.Id} name={element.Name} price={element.MRP} imageUrl={element.Image} />
