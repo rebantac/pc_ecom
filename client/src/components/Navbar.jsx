b741e4510465b5c0576465483599f83e02267007
Mritunjay Kumar <83032964+Mritunjay1711@users.noreply.github.com>
2023-01-12 23:10:14 +0530

    bug fix

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,38 +4,6 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    // <div>
-    //   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    //     <div className="container-fluid">
-    //       <a className="navbar-brand" href="/">
-    //         <img className='mx-3' style={{ height: '50px', width: '50px', borderRadius: '20px' }} src={logo} alt="logo" />
-    //       </a>
-    //       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-    //           <li className='nav-item'>Home</li>
-    //           <li className="nav-item">Laptop</li>
-    //           <li className="nav-item">Printer</li>
-    //           <li className="nav-item">Monitor</li>
-    //         </ul>
-    //       </div>
-    //       <div className="flex space-x-4">
-    //         <a href="/" className=" text-gray-300 px-3 py-2 rounded-md text-sm font-medium">
-    //           Why Choose Us
-    //         </a>
-    //         <a href="/" className=" text-gray-300 px-3 py-2 rounded-md text-sm font-medium">
-    //           Contact Us
-    //         </a>
-    //         <a href="/" className="  px-3 py-2 ">
-    //           Services
-    //         </a>
-    //         <a href="/" className=" text-gray-300 px-3 py-2 rounded-md text-sm font-medium">
-    //           Cart
-    //         </a>
-    //       </div>
-    //     </div>
-    //   </nav>
-    // </div>
-
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
