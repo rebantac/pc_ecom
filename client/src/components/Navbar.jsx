9b0d9bdb9bfc8b9186eedc7c382fded7ac983b7c
Divyansi Mishra <divyansim01@gmail.com>
2023-04-01 12:58:25 +0530

    update logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/"><img className='mx-3' style={{ height: '50px', width: '50px', borderRadius: '20px' }} src={logo} alt="logo" /></Link>
+          <Link className="navbar-brand" to="/"><img className='mx-3' style={{ height: '50px', width: '50px',  }} src={logo} alt="logo" /></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
