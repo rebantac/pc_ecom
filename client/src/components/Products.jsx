ccb7eefe4e46bab183e71d9564bf349be7994c7c
Rebanta Chakraborty <rebantachakraborty8@gmail.com>
2023-07-21 22:55:58 +0530

    new beginnings

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -4,21 +4,21 @@ import ProductCard from './ProductCard'
 // import {Link} from 'react-router-dom'
 
 function Products() {
-    return (
-        <div className="container my-3">
-            <div className="row">
-                <h1>Our Products</h1>
-                {data && data.map((element) => {
-                    // return <Link to={<ProductDetails key={element.Id}/>}><div className="col-md-3 my-3 mx-3">
-                    //     <ProductCard key={element.Id} name={element.Name} price={element.MRP} imageUrl={element.Image} />
-                    // </div></Link>
-                    return <div className="col-md-3 my-3 mx-3">
-                        <ProductCard key={element.Id} name={element.Name} price={element.MRP} imageUrl={element.Image} />
-                    </div>
-                })}
-            </div>
-        </div>
-    )
+  return (
+    <div className="container my-3">
+      <div className="row">
+        <h1>Our Products</h1>
+        {data && data.map((element) => {
+          // return <Link to={<ProductDetails key={element.Id}/>}><div className="col-md-3 my-3 mx-3">
+          //     <ProductCard key={element.Id} name={element.Name} price={element.MRP} imageUrl={element.Image} />
+          // </div></Link>
+          return <div className="col-md-3 my-3 mx-3">
+            <ProductCard key={element.Id} name={element.Name} price={element.MRP} imageUrl={element.Image} />
+          </div>
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Products
