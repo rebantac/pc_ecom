473ae98f75ce12e3bac27f576c171ae38951108a
Mritunjay Kumar <83032964+Mritunjay1711@users.noreply.github.com>
2023-07-29 23:03:16 +0530

    update product card

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import AboutUs from "./AboutUs";
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar w-full navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/"><img className='mx-3' style={{ height: '50px', width: '50px', }} src={logo} alt="logo" /></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
