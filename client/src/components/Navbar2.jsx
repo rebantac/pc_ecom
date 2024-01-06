1c1098a041b491c21989144ff71418e4e6a86305
Divyansi Mishra <divyansim01@gmail.com>
2024-01-06 10:59:27 +0530

    update navbar2

--- client/src/components/Navbar2.jsx
+++ client/src/components/Navbar2.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-const Navbar2 = ()=> {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: "gray" }}>
-                    <div className="collapse navbar-collapse container-fluid" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className='nav-item'><Link className='nav-link' to='/'>Laptops</Link></li>
-                            <li className='nav-item'><Link className='nav-link' to='/'>Printers</Link></li>
-                            <li className='nav-item'><Link className='nav-link' to='/'>Monitors</Link></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        )
+// src/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Navbar2 = () => {
+    const categories = [
+        'Laptops',
+        'Monitors',
+        'Printers',
+        'Peripherals',
+        'CCTV Cameras',
+        'Services',
+        'LED Wall Installation',
+    ];
+
+    return (
+        <nav className="bg-gray-800 ">
+            <div className="container mx-auto">
+                    <ul className="flex space-x-2 text-white">
+                        {categories.map((category, index) => (
+                            <li key={index} className="relative group border-2 border-transparent hover:border-white p-2.5">
+
+                                <Link to="#" className='hover:text-white'>{category}</Link>
+                                {/* <div className="hidden group-hover:block absolute bg-white mt-2 p-4 rounded shadow-lg"> */}
+                                {/* Subcategories or additional content can be added here */}
+                                {/* Example: <p>Subcategory 1</p> */}
+                                {/* </div> */}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+        </nav>
+    );
 };
 
 export default Navbar2;
